76d8c910c68600279846dd4e28e400d200278442
John Doe <juangnba@hotmail.com>
2023-04-26 22:39:39 +0200

    fixed problems with cache

--- pages/api/avo/[id].jsx
+++
@@ -1,9 +0,0 @@
-import Database from "../../../database/db";
-
-const allAvos = async (req, res) => {
-    const { id } = req.query;
-    const data = await Database.getById(id);
-    res.status(200).json({ data, length: data.length });
-};
-
-export default allAvos;
--- pages/api/avo/index.jsx
+++
@@ -1,8 +0,0 @@
-import Database from "../../../database/db";
-
-const allAvos = async (req, res) => {
-    const data = await Database.getAll();
-    res.status(200).json({ data, length: data.length });
-};
-
-export default allAvos;
