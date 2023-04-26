b1d24886598d7ba7c06f05f2f80281b3a4992605
John Doe <juangnba@hotmail.com>
2023-04-26 22:30:45 +0200

    added _document

--- database/db.js
+++ database/db.js
@@ -4,21 +4,12 @@ const Database = {};
 
 Database.getAll = async () => {
     const asArray = Object.values(data);
-    await randomDelay();
     return asArray;
 };
 
 Database.getById = async (id) => {
     const entry = data[id];
-    await randomDelay();
     return entry;
-};
-
-const randomDelay = () => {
-    return new Promise((resolve) => {
-        const delay = Math.floor(Math.random() * 4000) + 1000;
-        setTimeout(resolve, delay);
-    });
 };
 
 export default Database;
